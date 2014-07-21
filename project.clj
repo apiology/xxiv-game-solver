60219efb3d3282cb555dfdf86ffebbcd6a5ce89e
Jason Gilman <jason@element84.com>
2014-07-20 21:29:55 -0400

    Implemented first solution.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/test.check "0.5.8"]
+                 [org.clojure/math.combinatorics "0.0.8"]]
   :profiles 
   {:dev {:source-paths ["dev" "src"]
          :dependencies [[org.clojure/tools.namespace "0.2.4"]
--- src/xxiv_game_solver/core.clj
+++ src/xxiv_game_solver/core.clj
@@ -1,6 +1,62 @@
-(ns xxiv-game-solver.core)
+(ns xxiv-game-solver.core
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.math.combinatorics :as combo]
+            [clojure.walk :as w]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def nums
+  (gen/choose 1 9))
+
+(def num-lists
+  (gen/vector nums 4))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; my implementation
+
+(combo/permutations [7 4 3 1])
+
+(def possible-operations
+  [+ * / -]
+  ;; easier printing for debugging
+  ; [:+ :* :/ :-]
+  )
+
+(defn pair-combinations
+  [as b]
+  (mapcat (fn [a]
+         (map (fn [op] (list op a b)) possible-operations))
+       as))
+
+(defn nums->operations
+  [nums]
+  (mapcat (fn [[a b c d]]
+            (-> [a]
+                (pair-combinations b)
+                (pair-combinations c)
+                (pair-combinations d)))
+          (combo/permutations nums)))
+
+(defn solve-xxiv
+  "TODO"
+  [nums]
+  (filter (fn [op]
+            (= 24 (eval op)))
+          (nums->operations nums)))
+
+(defn pretty-print-equation
+  [op]
+  (w/prewalk-replace {* :* + :+ - :- / :/} op))
+(comment 
+(map pretty-print-equation (solve-xxiv [1 3 1 4]))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn evaluate-solver
+  []
+  (let [num-lists (gen/sample-seq 100)]
+    ;; should iterate over each num list evaluating the results 
+    ;; make sure it is 24
+    ;; Count the number of requests that it can find 24
+    
+    ))
