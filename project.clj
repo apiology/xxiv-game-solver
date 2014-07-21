50423bce23b4dacd24ee458a9e27938c47e34639
Jason Gilman <jason@element84.com>
2014-07-20 20:17:54 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject xxiv_game_solver "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :profiles 
+  {:dev {:source-paths ["dev" "src"]
+         :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                        [org.clojars.gjahad/debug-repl "0.3.3"]]}})
---
+++ test/xxiv_game_solver/core_test.clj
@@ -0,0 +1,7 @@
+(ns xxiv-game-solver.core-test
+  (:require [clojure.test :refer :all]
+            [xxiv-game-solver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
