124227e539a10bf70f05dcd7122d8ee9d317e027
Vince Broz <vince@broz.cc>
2014-07-22 19:24:01 -0400

    Add my solution

--- src/xxiv_game_solver/core.clj
+++ src/xxiv_game_solver/core.clj
@@ -2,34 +2,32 @@
   (:require [clojure.math.combinatorics :as combo]
             [clojure.walk :as w]))
 
-(comment 
-  
-  ;; an example of the 24 game
-  (let [n1 1
-        n2 2
-        n3 3
-        n4 4
-        
-        ;; ` is the syntax quote
-        ;; ~ is unqote
-        ;; For what those do see: http://yobriefca.se/blog/2014/05/19/the-weird-and-wonderful-characters-of-clojure/ 
-        ;; "operation" will now contain a list containing some clojure functions and the the values 
-        ;; in n1, n2, n3, and n4. This is a data structure that can be evaluated as clojure code.
-        operation `(* (+ ~n1 ~n2 ~n3) ~n4)]
-    
-    ;; We can evaluate operation as clojure code.
-    (eval operation))
-  
-  )
+(defn all-number-permutations [n1 n2 n3 n4]
+  (combo/permutations [n1 n2 n3 n4]))
 
+(def all-operator-options
+  (combo/selections [+ - / *] 3))
+
+(defn create-expression [[n1 n2 n3 n4] [o1 o2 o3]]
+    (list o1 n1 (list o2 n2 (list o3 n3 n4))))
+
+(defn possible-answers [n1 n2 n3 n4]
+  (for [numbers (all-number-permutations n1 n2 n3 n4)
+        operators all-operator-options]
+    (create-expression numbers operators)))
+
+(defn evals-to-24? [l]
+  (try
+    (= 24 (eval l))
+    (catch java.lang.ArithmeticException e
+      false)))
 
 (defn solve-xxiv
-  "Takes a list of 4 numbers. It should see if the 4 numbers can be combined mathematically using 
-  +, *, -, and / to somehow equal the sum 24. It should return a sequence of Clojure code combining
-  n1, n2, n3, and n4 with the 4 operations that evaluate to 24."
+  "Takes a list of 4 numbers. It should see if the 4 numbers can be
+  combined mathematically using +, *, -, and / to somehow equal the
+  sum 24. It should return a sequence of Clojure code combining n1,
+  n2, n3, and n4 with the 4 operations that evaluate to 24."  
   [n1 n2 n3 n4]
-  
-  ;; TODO implement this.
-  
-  )
+  (filter evals-to-24? (possible-answers n1 n2 n3 n4)))
+
 
