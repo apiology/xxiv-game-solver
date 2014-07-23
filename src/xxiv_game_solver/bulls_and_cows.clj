a8d4c776138758e2df4e7341688a68f1c8df594c
Vince Broz <vince@broz.cc>
2014-07-23 08:16:20 -0400

    Remove debugging

--- src/xxiv_game_solver/bulls_and_cows.clj
+++ src/xxiv_game_solver/bulls_and_cows.clj
@@ -4,53 +4,56 @@
   (:require [clojure.set :as set]
             [clojure.math.combinatorics :as combo]))
 
-(def possible-guesses
-  (let [possible-nums (range 10)]
-    (for [n1 possible-nums
-          n2 possible-nums
-          n3 possible-nums
-          n4 possible-nums
-          :when (and (not= n2 n1)
-                     (not= n3 n2)
-                     (not= n3 n1)
-                     (not= n4 n3)
-                     (not= n4 n2)
-                     (not= n4 n1))]
-      [n1 n2 n3 n4])))
+(declare count-cows)
+(declare count-bulls)
 
+(defn- count-true-cows
+  [secret-nums guess-nums]
+  (- (count-cows secret-nums guess-nums) 
+     (count-bulls secret-nums guess-nums)))
 
+(defn same-result? [secret-nums result]
+  (let [[guess-nums bulls cows] result]
+    (and (= bulls (count-bulls secret-nums guess-nums))
+         (= cows (count-true-cows secret-nums guess-nums)))))
 
-
-(defn same-result? [secret-nums [guess-nums bulls cows]]
-  (and (= bulls (count-bulls secret-nums guess-nums))
-       (= cows (count-cows secret-nums guess-nums))))
 
 (defn could-be-solution? [results-so-far potential-solution]
   (every? #(same-result? potential-solution %) results-so-far))
 
-(defn solve-bulls-and-cows
-  "Implements playing the bulls and cows game. The guess-fn takes 4
+(def all-possible-answers
+  (shuffle
+   (let [possible-nums (range 10)]
+     (for [n1 possible-nums
+           n2 possible-nums
+           n3 possible-nums
+           n4 possible-nums
+           :when (and (not= n2 n1)
+                      (not= n3 n2)
+                      (not= n3 n1)
+                      (not= n4 n3)
+                      (not= n4 n2)
+                      (not= n4 n1))]
+       [n1 n2 n3 n4]))))
+
+  (defn solve-bulls-and-cows
+    "Implements playing the bulls and cows game. The guess-fn takes 4
   numbers and returns a tuple of the number of bulls and cows. 'Bulls'
   are exact matches (position and number). 'Cows' and numbers that
   match position but not the correct index. It should return the
   matching numbers."
-  [guess-fn]
-  ;; TODO implement this
-  ;;
-  ;; each time I go through I get # of bulls and cows.
-  ;;
-  ;; I can filter further solutions to those that have the same # of
-  ;; bulls and cows.
-  (loop [all-answers (combo/combinations (range 9) 4)
-         results-so-far '()]
-    (let [current-answer (first all-answers)
-          [bulls cows] (guess-fn current-answer)
-          results-after-this-guess (cons (list current-answer bulls cows))]
-      (if (= bulls 4)
-        current-answer
-        (recur (filter (partial could-be-solution? results-so-far)
-                       (rest all-answers))
-               (cons results-after-this-guess [current-answer bulls cows]))))))
+    [guess-fn]
+    (loop [all-answers all-possible-answers
+           results-so-far []]
+      (let [current-answer (first all-answers)
+            [bulls cows] (guess-fn current-answer)
+            results-after-this-guess (conj results-so-far 
+                                           (list current-answer bulls cows))]
+        (if (= bulls 4)
+          current-answer
+          (recur (filter #(could-be-solution? results-so-far %)
+                         (rest all-answers))
+                 results-after-this-guess)))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -90,3 +93,4 @@
     (= answer secret-nums)))
 
 
+(test-solver [5 2 9 1])
