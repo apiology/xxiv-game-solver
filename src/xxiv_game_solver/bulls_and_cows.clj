6484963cc08b23a32e31678b650e2dc0fd8ca554
Vince Broz <vince@broz.cc>
2014-07-22 20:10:30 -0400

    Start of solution

--- src/xxiv_game_solver/bulls_and_cows.clj
+++ src/xxiv_game_solver/bulls_and_cows.clj
@@ -1,16 +1,41 @@
 (ns xxiv-game-solver.bulls-and-cows
   "A bonus problem to solve for Clojure DC meetup.
   See http://en.wikipedia.org/wiki/Bulls_and_cows for a description of the game."
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
 
+
+(defn same-result? [secret-nums [guess-nums bulls cows]]
+  (and (= bulls (count-bulls secret-nums guess-nums))
+       (= cows (count-cows secret-nums guess-nums))))
+
+(defn could-be-solution? [results-so-far potential-solution]
+  (every? #(same-result? potential-solution %) results-so-far))
 
 (defn solve-bulls-and-cows
-  "Implements playing the bulls and cows game. The guess-fn takes 4 numbers and returns a tuple
-  of the number of bulls and cows. 'Bulls' are exact matches (position and number). 'Cows' and numbers
-  that match position but not the correct index. It should return the matching numbers."
+  "Implements playing the bulls and cows game. The guess-fn takes 4
+  numbers and returns a tuple of the number of bulls and cows. 'Bulls'
+  are exact matches (position and number). 'Cows' and numbers that
+  match position but not the correct index. It should return the
+  matching numbers."
   [guess-fn]
   ;; TODO implement this
-  )
+  ;;
+  ;; each time I go through I get # of bulls and cows.
+  ;;
+  ;; I can filter further solutions to those that have the same # of
+  ;; bulls and cows.
+  (loop [all-answers (combo/combinations (range 9) 4)
+         results-so-far '()]
+    (let [current-answer (first all-answers)
+          [bulls cows] (guess-fn current-answer)
+          results-after-this-guess (cons (list current-answer bulls cows))]
+      (if (= bulls 4)
+        current-answer
+        (recur (filter (partial could-be-solution? results-so-far)
+                       (rest all-answers))
+               (cons results-after-this-guess [current-answer bulls cows]))))))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,7 +51,8 @@
                  (map #(vector %1 %2) secret-nums guess-nums))))
 
 (defn guess
-  "Checks which of the guess nums are bulls and cows. Returns a tuple of the number of bulls and cows."
+  "Checks which of the guess nums are bulls and cows. Returns a tuple
+   of the number of bulls and cows."
   [secret-nums guess-nums]
   
   (when (not= (count (distinct secret-nums)) 4)
