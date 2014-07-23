67e2876e452fb09e1fe460b7a1939428e9743fe7
Jason Gilman <jason@element84.com>
2014-07-22 20:09:48 -0400

    Added counting for bulls and cows.

--- src/xxiv_game_solver/bulls_and_cows.clj
+++ src/xxiv_game_solver/bulls_and_cows.clj
@@ -1,7 +1,24 @@
 (ns xxiv-game-solver.bulls-and-cows
   "A bonus problem to solve for Clojure DC meetup.
   See http://en.wikipedia.org/wiki/Bulls_and_cows for a description of the game."
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
+
+(def possible-guesses
+  (let [possible-nums (range 10)]
+    (for [n1 possible-nums
+          n2 possible-nums
+          n3 possible-nums
+          n4 possible-nums
+          :when (and (not= n2 n1)
+                     (not= n3 n2)
+                     (not= n3 n1)
+                     (not= n4 n3)
+                     (not= n4 n2)
+                     (not= n4 n1))]
+      [n1 n2 n3 n4])))
+
+
 
 
 (defn solve-bulls-and-cows
@@ -9,8 +26,23 @@
   of the number of bulls and cows. 'Bulls' are exact matches (position and number). 'Cows' and numbers
   that match position but not the correct index. It should return the matching numbers."
   [guess-fn]
-  ;; TODO implement this
-  )
+
+  (->> possible-guesses
+       (map (fn [nums]
+              [(guess-fn nums) nums]))
+       (filter (fn [[[bulls cows] nums]]
+                 (= bulls 4)))
+       (map second)
+       first))
+
+(comment
+  (solve-bulls-and-cows (partial guess [1 2 3 4]))
+  (test-solver [4 2 3 5])
+  (test-solver [1 2 3 4])
+  (test-solver [0 1 2 3])
+)
+
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -28,20 +60,24 @@
 (defn guess
   "Checks which of the guess nums are bulls and cows. Returns a tuple of the number of bulls and cows."
   [secret-nums guess-nums]
-  
+
   (when (not= (count (distinct secret-nums)) 4)
     (throw (Exception. "Secret nums must be a set of 4 distinct numbers")))
   (when (not= (count (distinct guess-nums)) 4)
     (throw (Exception. "A guess must be a set of 4 distinct numbers")))
-  
+
   (let [num-bulls (count-bulls secret-nums guess-nums)]
     [num-bulls (- (count-cows secret-nums guess-nums) num-bulls)]))
 
 (defn test-solver
   "Tests the bulls and cows solver using the given number. Returns true if it finishes."
   [secret-nums]
-  (let [guess-fn (partial guess secret-nums)
+  (let [counter (atom 0)
+        guess-fn (fn [nums]
+                   (swap! counter inc)
+                   (guess secret-nums nums))
         answer (solve-bulls-and-cows guess-fn)]
+    (println "Number of guesses" @counter)
     (= answer secret-nums)))
 
 
--- src/xxiv_game_solver/core.clj
+++ src/xxiv_game_solver/core.clj
@@ -2,34 +2,56 @@
   (:require [clojure.math.combinatorics :as combo]
             [clojure.walk :as w]))
 
-(comment 
-  
+(comment
+
   ;; an example of the 24 game
   (let [n1 1
         n2 2
         n3 3
         n4 4
-        
+
         ;; ` is the syntax quote
         ;; ~ is unqote
-        ;; For what those do see: http://yobriefca.se/blog/2014/05/19/the-weird-and-wonderful-characters-of-clojure/ 
-        ;; "operation" will now contain a list containing some clojure functions and the the values 
+        ;; For what those do see: http://yobriefca.se/blog/2014/05/19/the-weird-and-wonderful-characters-of-clojure/
+        ;; "operation" will now contain a list containing some clojure functions and the the values
         ;; in n1, n2, n3, and n4. This is a data structure that can be evaluated as clojure code.
         operation `(* (+ ~n1 ~n2 ~n3) ~n4)]
-    
+
     ;; We can evaluate operation as clojure code.
     (eval operation))
-  
+
+  '(* (+ 1 2 3) 4)
+
+  (* 8 (/ 8 6) )
+
   )
 
+(def permitted-operations
+   ['+ '- '/ '*])
+
+(defn combine-forms
+  [a b]
+  (map (fn [op]
+       (list op a b))
+     permitted-operations))
+
+(defn combine-numbers
+  [[n1 n2 n3 n4]]
+  (->> (combine-forms n1 n2)
+       (mapcat #(combine-forms % n3))
+       (mapcat #(combine-forms % n4))))
+
+(defn correct-answer?
+  [op]
+  (= (eval op) 24))
 
 (defn solve-xxiv
-  "Takes a list of 4 numbers. It should see if the 4 numbers can be combined mathematically using 
+  "Takes a list of 4 numbers. It should see if the 4 numbers can be combined mathematically using
   +, *, -, and / to somehow equal the sum 24. It should return a sequence of Clojure code combining
   n1, n2, n3, and n4 with the 4 operations that evaluate to 24."
   [n1 n2 n3 n4]
-  
-  ;; TODO implement this.
-  
-  )
+
+  (->> (combo/permutations [n1 n2 n3 n4])
+       (mapcat combine-numbers)
+       (filter correct-answer?)))
 
